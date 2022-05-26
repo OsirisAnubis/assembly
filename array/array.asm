8a5d900a5d61921b84c57de4688795e0e6353090
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-05-26 19:07:06 +0300

    remove non-english language

--- array/array.asm
+++ array/array.asm
@@ -1,12 +1,12 @@
 section .bss
-array	resb 256	;массив размером 256 байт
+array	resb 256	
 
 section .text
 ;...
-	mov ecx, 256	;кол-во элементов -> в счётчик ecx
-	mov edi, array	;адрес масива -> в edi
-	mov al, '@'	;нужный код  -> в однобайтовый al
-again:	mov [edi], al	;заносим в очередной элемент
-	inc edi		;увеличиваем адрес
-	dec ecx		;уменьшаем счётчик
-	jnz again	;если там не ноль, повторяем цикл
+	mov ecx, 256	
+	mov edi, array	
+	mov al, '@'	
+again:	mov [edi], al	
+	inc edi		
+	dec ecx		
+	jnz again	
